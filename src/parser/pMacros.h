158efa5da0dae3c01fc74feaed935279ab021577
KitiHey <tomoiagalaurentiu2017@gmail.com>
2025-08-31 15:46:50 +0200

    Add Error Macros

--- src/parser/pMacros.h
+++ src/parser/pMacros.h
@@ -11,3 +11,14 @@
 #define FREE(Arena) ArenaFree(Arena)
 #define STRLEN(character) (strlen(character) + 1)
 #define STRDUP(character) Strdup(Arena, character);
+
+// Errors
+#define ERROR_EQ(str, ...) ERROR(str, ##__VA_ARGS__);
+#define ERROR_NEQ(str, ...) ERROR(str, ##__VA_ARGS__);
+#define ERROR_NULL(str, ...) ERROR(str, ##__VA_ARGS__);
+
+#define ERROR_IFNEQ(ifneq, expected, str, ...) if (ifneq != expected) { ERROR_NEQ(str, ##__VA_ARGS__); return NULL; }
+#define ERROR_IFEQ(ifeq, expected, str, ...) if (ifeq == expected) { ERROR_EQ(str, ##__VA_ARGS__); return NULL; }
+#define ERROR_IFNULL(isnull, str, ...) if (isnull == NULL) { ERROR_NULL(str, ##__VA_ARGS__); return NULL; }
+
+#define ERROR(str, ...) printf("PARSER ERROR: " str "\n", ##__VA_ARGS__)
