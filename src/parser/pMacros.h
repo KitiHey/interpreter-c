8c539f887817055505fca0fec250d4b96027c14e
KitiHey <tomoiagalaurentiu2017@gmail.com>
2025-08-31 16:29:30 +0200

    Add Malloc Error

--- src/parser/pMacros.h
+++ src/parser/pMacros.h
@@ -16,9 +16,11 @@
 #define ERROR_EQ(str, ...) ERROR(str, ##__VA_ARGS__);
 #define ERROR_NEQ(str, ...) ERROR(str, ##__VA_ARGS__);
 #define ERROR_NULL(str, ...) ERROR(str, ##__VA_ARGS__);
+#define ERROR_MALLOC(str, ...) ERROR(str, ##__VA_ARGS__);
 
 #define ERROR_IFNEQ(ifneq, expected, str, ...) if (ifneq != expected) { ERROR_NEQ(str, ##__VA_ARGS__); return NULL; }
 #define ERROR_IFEQ(ifeq, expected, str, ...) if (ifeq == expected) { ERROR_EQ(str, ##__VA_ARGS__); return NULL; }
 #define ERROR_IFNULL(isnull, str, ...) if (isnull == NULL) { ERROR_NULL(str, ##__VA_ARGS__); return NULL; }
+#define ERROR_IFMALLOC(isnull, str, ...) if (isnull == NULL) { ERROR_MALLOC(str, ##__VA_ARGS__); return NULL; }
 
 #define ERROR(str, ...) printf("PARSER ERROR: " str "\n", ##__VA_ARGS__)
